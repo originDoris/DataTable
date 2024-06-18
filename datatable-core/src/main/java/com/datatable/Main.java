3ddfc9a0e82bbdd0ecc79a36c29e40dce6b79313
xhz <origindoris@dingtalk.com>
2024-06-18 19:23:20 +0800

    初始化 core项目

---
+++ datatable-core/src/main/java/com/datatable/Main.java
@@ -0,0 +1,12 @@
+package com.datatable;
+
+/**
+ * 表达式解析器
+ *
+ * @author xhz
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
